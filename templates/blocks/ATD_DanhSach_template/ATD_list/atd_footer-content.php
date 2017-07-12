7a0efc7f1891b51a33b1d63dc4bf2823574c8d90
theworld520131415 <theworld52013141516@gmail.com>
2017-07-13 02:40:04 +0700

    ATD_List - completed

---
+++ templates/blocks/ATD_DanhSach_template/ATD_list/atd_footer-content.php
@@ -0,0 +1,53 @@
+<?php
+    $url_host = $_SERVER['HTTP_HOST'];
+
+    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+
+    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+
+$url_path = $url_host . $matches[1][0];
+
+$url_path = str_replace('\\', '/', $url_path);
+?>
+
+       <div class="atd_footer">
+            <footer class="footer-component">
+                <div class="row mega-container">
+                    <div class="col-xs-12 col-sm-3 col-sm-push-9">
+                        <ul class="text-right list-inline social">
+                            <li class="block socialbuttonblock  "><i class="fa fa-facebook-official" aria-hidden="true"></i>
+                            </li>
+                            <li class="block socialbuttonblock  "><i class="fa fa-twitter" aria-hidden="true"></i>
+                            </li>
+                            <li class="block socialbuttonblock  "><i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                            </li>
+                            <li class="block socialbuttonblock  "><i class="fa fa-youtube-play" aria-hidden="true"></i>
+                            </li>
+                            <li class="block socialbuttonblock  "><i class="fa fa-google-plus" aria-hidden="true"></i>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-3 col-sm-pull-3">
+                        <p>&#169;2017 Vertiv Co. All rights reserved.</p>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-sm-pull-3">
+                        <ul class="text-center list-inline">
+                            <li><a href="#">Terms of Use</a></li>
+                            <li class="dot">&#8226;</li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li class="dot">&#8226;</li>
+                            <li><a href="#">Site Map</a></li>
+                            <li class="dot">&#8226;</li>
+                            <li><a data-toggle="modal" data-target="#modal-subscribe" id="subscribeFormLauncher" href="#">Subscribe</a></li>
+                            <li class="dot">&#8226;</li>
+                            <li><a data-toggle="modal" data-target="#modal-feedback" id="feedbackForm" href="#">Feedback</a></li>
+                        </ul>
+                    </div>
+                </div>
+                  
+            </footer>
+       
+        </div>
+  <div class="endweb"></div>
---
+++ templates/blocks/ATD_DanhSach_template/ATD_list/atd_list.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <?php
+        if (!class_exists('lessc')) {
+            include ('libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/atd_list.less', 'css/atd_list.css');
+        ?> 
+        
+        <link href="css/atd_list.css" rel="stylesheet" type="text/css"/>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        
+        <link href="css/owl.theme.css" rel="stylesheet" type="text/css"/>
+        <link href="css/owl.carousel.css" rel="stylesheet" type="text/css"/>
+        
+        
+         <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="js/owl.carousel.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap-offcanvas-navigation.js" type="text/javascript"></script>
+       
+       
+        <script src="js/scrolltotop.js" type="text/javascript"></script>
+        <script src="js/module_menu.js" type="text/javascript"></script>
+        <script src="js/slideshowDS.js" type="text/javascript"></script>
+        <script src="js/click.js" type="text/javascript"></script>
+        
+        
+    </head> 
+
+    <body> 
+                <?php include '../ATD_list/module_menu-content.php'; ?>
+                <?php include '../ATD_list/atd_module1-content.php'; ?>
+                <?php include '../ATD_list/atd_module2-content.php'; ?>
+                <?php include '../ATD_list/atd_module3-content.php'; ?>
+                <?php include '../ATD_list/atd_module4-content.php'; ?>
+                <?php include '../ATD_list/atd_footer-content.php'; ?>
+                <?php include '../ATD_list/atd_phutro-content.php'; ?>
+       
+    </body>
+</html>
