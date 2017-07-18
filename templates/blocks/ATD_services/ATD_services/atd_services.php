cd467a1da43212c2cf72741dabf001ae088c3587
theworld520131415 <theworld52013141516@gmail.com>
2017-07-19 05:12:40 +0700

    ATD_services-complete

---
+++ templates/blocks/ATD_services/ATD_services/atd_services.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <?php
+        if (!class_exists('lessc')) {
+            include ('libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/atd_services.less', 'css/atd_services.css');
+        ?> 
+        
+        <link href="css/atd_services.css" rel="stylesheet" type="text/css"/>
+        
+        
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        
+       
+        
+        
+        
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript">
+        </script><script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/owl.carousel.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap-offcanvas-navigation.js" type="text/javascript"></script>
+       
+       
+        <script src="js/scrolltotop.js" type="text/javascript"></script>
+        <script src="js/module_menu.js" type="text/javascript"></script>
+        <script src="js/slideshowservices.js" type="text/javascript"></script>
+        <script src="js/scrolltotopconten2.js" type="text/javascript"></script>
+        
+    </head> 
+
+    <body > 
+        <div class="backgroud">
+                <?php include '../ATD_services/module_menu-content.php'; ?>
+                <?php include '../ATD_services/1_content.php';?>
+                <?php include '../ATD_services/services-2-content.php';?>
+                <?php include '../ATD_services/services-3-content.php';?>
+                <?php include '../ATD_services/4-content.php';?>
+                <?php include '../ATD_services/atd_module4-content.php';?>
+                <?php include '../ATD_services/atd_footer-content.php'; ?>
+                <?php include '../ATD_services/atd_phutro-content.php'; ?>
+       </div>
+    </body>
+</html>
---
+++ templates/blocks/ATD_services/ATD_services/js/module_menu.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $(".search-tablet-trigger").click(function () {
+        $(".navbar-form").slideToggle();
+    });
+
+});
+$(document).ready(function () {
+    $(".input-group-btn").click(function () {
+        $(".dropdown-menu").slideToggle();
+    });
+
+});
+$(document).ready(function () {
+    $(".search-trigger").click(function () {
+        $(".navbar-form").slideToggle();
+    });
+});
+
