dbcb758e25413ee9f6e4e0cc152db920643d1dbd
NguyenVanLuong <luongdia16@gmail.com>
2017-07-22 01:50:09 +0700

    luong

---
+++ templates/blocks/ATD_Insights/3/js/3.js
@@ -0,0 +1,8 @@
+$(function (){
+   $("#show8More").click(function(){
+       $(".show-more").css('display','block'); 
+   });
+   $("#show8More").click(function(){
+       $(".show-more2").css('display','block'); 
+   });
+});
