bcc6f26446833a1c786f8d8e5ef2f0709d5aaf1a
theworld520131415 <theworld52013141516@gmail.com>
2017-07-30 23:41:42 +0700

    change

--- templates/blocks/4001/4001-content.php
+++ templates/blocks/4001/4001-content.php
@@ -1,9 +1,9 @@
 <?php
-    $url_host = $_SERVER['HTTP_HOST'];
+$url_host = $_SERVER['HTTP_HOST'];
 
-    $pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
 
-    $pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
 
 preg_match_all($pattern_uri, __DIR__, $matches);
 
@@ -13,75 +13,76 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 
 <div class="module-4001">
-  
-    <div class="banner1">
-        <div class="mod_custom_7">
-            <div class="sld-hover">
-                <div id="owl-demo">
 
-                    <div class="item">
-                        <div class="slide-content">
-                            <span class="subject" style="color: #FFFFFF !important;">
-            RANKING THE WORLD'S MOST CRITICAL INDUSTRIES
-        </span>
-                            <p class="hero-title" style="color: #FFFFFF !important;">
-                                Evaluating the impact of disruption
-                            </p>
-                            <a href="#" target="_parent" class="button primary" tabindex="0">
-            Read the report
-        </a>
-                        </div>
-                        <img src="image/h1.jpg" alt="The Last of us">
-                    </div>
 
-                    <div class="item">
-                        <div class="slide-content">
-                            <span class="subject" style="color: #FFFFFF !important;">
-            RANKING THE WORLD'S MOST CRITICAL INDUSTRIES
-        </span>
-                            <p class="hero-title" style="color: #FFFFFF !important;">
-                                Evaluating the impact of disruption
-                            </p>
-                            <a href="#" target="_parent" class="button primary" tabindex="0">
-            Read the report
-        </a>
-                        </div>
-                        <img src="image/h2.jpg" alt="GTA V">
-                    </div>
+   
+    <div class="sld-hover">
+        <div id="owl-demo">
 
-                    <div class="item">
-                        <div class="slide-content">
-                            <span class="subject" style="color: #FFFFFF !important;">
-            RANKING THE WORLD'S MOST CRITICAL INDUSTRIES
-        </span>
-                            <p class="hero-title" style="color: #FFFFFF !important;">
-                                Evaluating the impact of disruption
-                            </p>
-                            <a href="#" target="_parent" class="button primary" tabindex="0">
-            Read the report
-        </a>
-                        </div>
-                        <img src="image/h3.jpg" alt="Mirror Edge">
-                    </div>
-                    <div class="item">
-                        <div class="slide-content">
-                            <span class="subject" style="color: #FFFFFF !important;">
-            RANKING THE WORLD'S MOST CRITICAL INDUSTRIES
-        </span>
-                            <p class="hero-title" style="color: #FFFFFF !important;">
-                                Evaluating the impact of disruption
-                            </p>
-                            <a href="#" target="_parent" class="button primary" tabindex="0">
-            Read the report
-        </a>
-                        </div>
-                        <img src="image/h4.jpg" alt="Mirror Edge">
-                    </div>
-
+            <div class="item">
+                <div class="slide-content">
+                    <span class="subject" >
+                        RANKING THE WORLD'S MOST CRITICAL INDUSTRIES
+                    </span>
+                    <p class="hero-title" >
+                        Evaluating the impact of disruption
+                    </p>
+                    <a href="#"  class="button primary" >
+                        Read the report
+                    </a>
                 </div>
+                <img class="img-responsive" src="image/h1.jpg" alt="">
             </div>
-        </div>
 
+            <div class="item">
+                <div class="slide-content">
+                    <span class="subject" >
+                        RANKING THE WORLD'S MOST CRITICAL INDUSTRIES
+                    </span>
+                    <p class="hero-title" >
+                        Evaluating the impact of disruption
+                    </p>
+                    <a href="#" class="button">
+                        Read the report
+                    </a>
+                </div>
+                <img class="img-responsive" src="image/h2.jpg" alt="">
+            </div>
+
+            <div class="item">
+                <div class="slide-content">
+                    <span class="subject" >
+                        RANKING THE WORLD'S MOST CRITICAL INDUSTRIES
+                    </span>
+                    <p class="hero-title" >
+                        Evaluating the impact of disruption
+                    </p>
+                    <a href="#" class="button primary">
+                        Read the report
+                    </a>
+                </div>
+                <img class="img-responsive" src="image/h3.jpg" alt="">
+            </div>
+            <div class="item">
+                <div class="slide-content">
+                    <span class="subject" >
+                        RANKING THE WORLD'S MOST CRITICAL INDUSTRIES
+                    </span>
+                    <p class="hero-title" >
+                        Evaluating the impact of disruption
+                    </p>
+                    <a href="#"  class="button primary" >
+                        Read the report
+                    </a>
+                </div>
+                <img class="img-responsive" src="image/h4.jpg" alt="">
+            </div>
+
+        </div>
     </div>
 
-</div>
+
+
+
+
+    </div>
