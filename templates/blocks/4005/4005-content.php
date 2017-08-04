602462384dcfcd458ea3b9d7a59e8cf28405a26f
theworld520131415 <theworld52013141516@gmail.com>
2017-08-04 23:13:36 +0700

    change-footer-08/04/2017

--- templates/blocks/4005/4005-content.php
+++ templates/blocks/4005/4005-content.php
@@ -11,34 +11,36 @@ $url_path = $url_host . $matches[1][0];
 
 $url_path = str_replace('\\', '/', $url_path);
 ?>
-<div class="footer">
-<footer class="footer-component">
-    <div class="row mega-container">
-        <div class="col-xs-12 col-sm-3 col-sm-push-9">
-            <ul class="text-right list-inline social"><li class="block socialbuttonblock  "><i class="fa fa-facebook-official" aria-hidden="true"></i>
-                </li><li class="block socialbuttonblock  "><i class="fa fa-twitter" aria-hidden="true"></i>
-                </li><li class="block socialbuttonblock  "><i class="fa fa-linkedin-square" aria-hidden="true"></i>
-                </li><li class="block socialbuttonblock  "><i class="fa fa-youtube-play" aria-hidden="true"></i>
-                </li><li class="block socialbuttonblock  "><i class="fa fa-google-plus" aria-hidden="true"></i>
-                </li></ul>
-        </div>
-        <div class="col-xs-12 col-sm-3 col-sm-pull-3">
-            <p>&#169;2017 Vertiv Co. All rights reserved.</p>
-        </div>
-        <div class="col-xs-12 col-sm-6 col-sm-pull-3">
-            <ul class="text-center list-inline">
-                <li><a href="/en-us/terms-of-use/">Terms of Use</a></li>
-                <li class="dot">&#8226;</li>
-                <li><a href="/en-us/privacy-policy/">Privacy Policy</a></li>
-                <li class="dot">&#8226;</li>
-                <li><a href="/en-us/site-map/">Site Map</a></li>
-                <li class="dot">&#8226;</li>
-                <li><a data-toggle="modal" data-target="#modal-subscribe" id="subscribeFormLauncher" href="#">Subscribe</a></li>
-                <li class="dot">&#8226;</li>
-                <li><a data-toggle="modal" data-target="#modal-feedback" id="feedbackForm" href="#">Feedback</a></li>
-            </ul>
+<div class="module-4005">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 col-sm-push-9">
+                <div class="icon">
+                    <ul>
+                        <li> <a href="#"><i class="fa fa-facebook-official" ></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter" ></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin-square" ></i></a></li>
+                        <li> <a href="#"><i class="fa fa-youtube-play"></i></a></li>
+                        <li> <a href="#"><i class="fa fa-google-plus"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-3 col-sm-pull-3">
+                <p>©2017 Vertiv Co. All rights reserved.</p>
+            </div>
+            <div class="col-xs-12 col-sm-6 col-sm-pull-3">
+                <div class="text">
+                    <a href="#">Terms of Use</a>
+                    <span>•</span>
+                    <a href="#">Privacy Policy</a>
+                    <span>•</span>
+                    <a href="#">Site Map</a>
+                    <span>•</span>
+                    <a href="#">Subscribe</a>
+                    <span>•</span>
+                    <a href="#">Feedback</a>
+                </div>
+            </div>
         </div>
     </div>
-
-</footer>
 </div>
